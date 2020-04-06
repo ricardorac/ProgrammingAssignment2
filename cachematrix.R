b5e9cd51668304954c432d56865926541dada13f
Ricardo Costa <ricardo.rac@gmail.com>
2020-04-06 11:33:42 -0300

    Completing the code for programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  slv <- NULL
+  set <- function(y) {
+    x <<- y
+    slv <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(s) slv <<- s
+  getsolve <- function() slv
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+  
+  
 }
 
 
@@ -12,4 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
